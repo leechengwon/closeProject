c5b9787e18c85d83c182e962dab1bff0f1d455cc
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-03 10:04:30 +0900

    초기세팅 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <>
-      <h1 className="font-tenada text-16px flex h-[100vh] w-[100%]  items-center justify-center bg-primaryColor">
+      <h1 className=" flex h-[100vh] w-[100%] items-center  justify-center bg-primaryColor text-16px">
         지갑 지켜 프로젝트 요진 청원 크로스
       </h1>
     </>
