12a58fc65d72b1807b783ec50041a6dfbc355664
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-03 10:42:24 +0900

    초기세팅 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,8 @@
 const Main = () => {
   return (
-    <div className="flex items-center  justify-center">
-      <h1 className="h-[100vh] w-[100%] bg-primaryColor ">
-        지갑 지켜 프로젝트 요진 청원 크로스
-      </h1>
-    </div>
+    <h1 className="flex h-[100vh] w-[100%]  items-center justify-center bg-primaryColor ">
+      지갑 지켜 프로젝트 요진 청원 크로스
+    </h1>
   );
 };
 
