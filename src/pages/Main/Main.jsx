ccb5386687bfe33acade2b7f074253af923bd98f
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-03 10:33:19 +0900

    초기세팅 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,15 @@
 const Main = () => {
   return (
-    <>
-      <h1 className=" flex h-[100vh] w-[100%] items-center  justify-center bg-primaryColor text-16px">
+    <div className="flex items-center  justify-center">
+      <h1 className="h-[100vh] w-[100%] bg-primaryColor ">
         지갑 지켜 프로젝트 요진 청원 크로스
       </h1>
-    </>
+      <p>메롱메롱청원</p>
+      <input
+        type="text"
+        className="border-black  h-[50px] w-[300px] border-[1px]"
+      />
+    </div>
   );
 };
 
