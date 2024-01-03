a292cb4e0e8af09f10cd9b30e32f470e7c3dd124
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-03 10:41:47 +0900

    초기세팅 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,11 +4,6 @@ const Main = () => {
       <h1 className="h-[100vh] w-[100%] bg-primaryColor ">
         지갑 지켜 프로젝트 요진 청원 크로스
       </h1>
-      <p>메롱메롱청원</p>
-      <input
-        type="text"
-        className="border-black  h-[50px] w-[300px] border-[1px]"
-      />
     </div>
   );
 };
