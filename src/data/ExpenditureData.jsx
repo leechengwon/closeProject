0e25c2e1f361096bc60942970e01f496151a7035
박요진 <syon013@naver.com>
2024-01-04 23:10:26 +0900

    📝 지출, 수입에 관련된 상수데이터 추가
    - Chip Component 에 사용

---
+++ src/data/ExpenditureData.jsx
@@ -0,0 +1,74 @@
+export const EXPENDITURE_DATA = [
+  {
+    id: 1,
+    text: '식비',
+    value: 'foodExpenses',
+    src: '../money-protector/images/Chip/chip_food.png',
+  },
+  {
+    id: 2,
+    text: '교통/차량',
+    value: 'transportationExpenses',
+    src: '../money-protector/images/Chip/chip_car.png',
+  },
+  {
+    id: 3,
+    text: '문화생활',
+    value: 'culturalLifeExpenses',
+    src: '../money-protector/images/Chip/chip_culture.png',
+  },
+  {
+    id: 4,
+    text: '마트/편의점',
+    value: 'martExpenses',
+    src: '../money-protector/images/Chip/chip_shop.png',
+  },
+  {
+    id: 5,
+    text: '패션/미용',
+    value: 'fashionExpenses',
+    src: '../money-protector/images/Chip/chip_fashion.png',
+  },
+  {
+    id: 6,
+    text: '생활용품',
+    value: 'dailySuppliesExpenses',
+    src: '../money-protector/images/Chip/chip_lifestyle.png',
+  },
+  {
+    id: 7,
+    text: '주거/통신',
+    value: 'housingExpenses',
+    src: '../money-protector/images/Chip/chip_information.png',
+  },
+  {
+    id: 8,
+    text: '건강',
+    value: 'healthExpenses',
+    src: '../money-protector/images/Chip/chip_health.png',
+  },
+  {
+    id: 9,
+    text: '교육',
+    value: 'educationExpenses',
+    src: '../money-protector/images/Chip/chip_education.png',
+  },
+  {
+    id: 10,
+    text: '경조사/회비',
+    value: 'celebrationExpenses',
+    src: '../money-protector/images/Chip/chip_celebration.png',
+  },
+  {
+    id: 11,
+    text: '부모님',
+    value: 'parentsExpenses',
+    src: '../money-protector/images/Chip/chip_parents.png',
+  },
+  {
+    id: 12,
+    text: '기타',
+    value: 'etcExpenses',
+    src: '../money-protector/images/Chip/chip_etc.png',
+  },
+];
