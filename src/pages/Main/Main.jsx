37ce9216b36ac102d6667b8eb8bce4823a0f0534
leechengwon <141094801+leechengwon@users.noreply.github.com>
2024-01-05 11:48:58 +0900

    테스트 코드 삭제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,6 @@
-import Tap from '../../components/Tap/Tap';
-
 const Main = () => {
   return (
-    <main className=" flex h-[100vh] w-[100%] items-center justify-center  ">
-      <Tap />
-    </main>
+    <main className=" flex h-[100vh] w-[100%] items-center justify-center  "></main>
   );
 };
 
