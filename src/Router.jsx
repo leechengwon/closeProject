d3b7a1d49e64254b9e4a4edb9d1555a97d1273be
박요진 <syon013@naver.com>
2024-01-05 14:07:02 +0900

    ✨ Footer Component 추가
    1. 라우터 경로 추가
    2. Footer 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollTop/ScrollTop';
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter basename="/money-protector">
       <ScrollToTop />
-      <Header></Header>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
